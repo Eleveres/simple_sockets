e4e4088ada0c0971d9a9cc3fafd434f86cb75228
Ravi Pousseur <ravip@Ravis-iMac.local>
2020-05-24 17:18:32 +0200

    added blocking option to the create_server functions

--- src/create_ipv4_server.c
+++ src/create_ipv4_server.c
@@ -1,7 +1,7 @@
 #include "../simple_socket.h"
 
 
-int32_t create_ipv4_server(uint16_t server_port, uint8_t sock_type)
+int32_t create_ipv4_server(uint16_t server_port, uint8_t sock_type, bool block)
 {
     /* create's a new socket file descriptor and binds it to the
     local IPv4 address of the computer */
@@ -15,6 +15,8 @@ int32_t create_ipv4_server(uint16_t server_port, uint8_t sock_type)
 
     if ((sock = socket(PF_INET, sock_type, IPPROTO_TCP)) < 0)
         return -1;
+    if (block == true)
+        fcntl(sock, F_SETFL, O_NONBLOCK);
     if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0)
         return -2;
     if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
