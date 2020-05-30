7eed27f88ad829cf3efa45671d8712d1e17ec0a9
Ravi Pousseur <ravip@Ravis-iMac.local>
2020-05-30 19:26:57 +0200

    fix types

--- src/sendall.c
+++ src/sendall.c
@@ -8,13 +8,15 @@ bool sendall(int32_t sock, void *buffer, int32_t len,
     the len parameter has been sent */
 
     uint8_t *buffer_ptr = (uint8_t *)buffer;
-    int16_t sent;
+    int32_t sent;
     int32_t total_sent = 0;
 
     while (total_sent < len) {
         if ((sent = sendto(sock, buffer_ptr, len, 0, 
-                                (struct sockaddr *)addr, addr_len)) < 0)
+                                (struct sockaddr *)addr, addr_len)) < 0) {
+            printf("%d\n", sent);
             return false;
+        }
         total_sent += sent;
         buffer_ptr += sent;
     }
