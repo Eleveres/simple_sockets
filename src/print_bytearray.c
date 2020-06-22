616320eac12058ca8f2d31e917d333bb8229d4e5
Ravi Pousseur <ravip@Ravis-iMac.local>
2020-06-22 19:00:13 +0200

    print removed delim

--- src/print_bytearray.c
+++ src/print_bytearray.c
@@ -6,8 +6,6 @@ void print_bytearray(void *array, uint64_t array_len, const char *mode)
     /* prints nicely a bytearray in different formats */
 
     uint8_t *bytearray = (uint8_t *)array;
-    char *delim = "-----------------------------"
-                    "---------------------";
 
     for (uint32_t i = 0; i < array_len; i++) {
         if (mode[0] == 'c') {
@@ -28,7 +26,6 @@ void print_bytearray(void *array, uint64_t array_len, const char *mode)
             printf("%4d", bytearray[i]);
         }
         if ((i + 1) % 10 == 0) printf("\n");
-        if ((i + 1) % 150 == 0) printf("%s\n", delim);
     }
     printf("\n");
 }
